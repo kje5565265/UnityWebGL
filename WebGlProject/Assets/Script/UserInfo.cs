25c08ac9cb888257c104d3f30bfaea58976f16a9
hans <hans@anotherworld.game>
2024-08-27 14:13:44 +0900

    build

--- WebGlProject/Assets/Script/UserInfo.cs
+++ WebGlProject/Assets/Script/UserInfo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class UserInfo : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _text_url;
     [SerializeField] private TextMeshProUGUI _text_ID;
     [SerializeField] private TextMeshProUGUI _text_Name;
     // Start is called before the first frame update
@@ -13,8 +14,9 @@ public class UserInfo : MonoBehaviour
     {
         // URL 파라미터에서 유저 ID와 이름 읽기
         string url = Application.absoluteURL;
-        string userId = GetUrlParameter(url, "userid");
-        string userName = GetUrlParameter(url, "username");
+        _text_url.text = url; 
+        string userId = GetUrlParameter(url, "user_id");
+        string userName = GetUrlParameter(url, "user_name");
         _text_ID.text = $"User ID: {userId}";
         _text_Name.text = $"User Name: {userName}";
     }
